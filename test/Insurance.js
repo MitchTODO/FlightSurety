74c46675c3444170106ec9adfea06d51957d8b43
Mitchell Tucker <tucker.mitchelltucker.mitchell@gmail.com>
2019-06-20 14:27:36 -0600

    Removed a test
    test was migrated to orcale.js

--- test/Insurance.js
+++ test/Insurance.js
@@ -42,25 +42,5 @@ contract('Insurance', async (accounts) => {
     assert.equal(result,true,"Consumer faild buying insurance");
   });
 
-  it('Buying flight Insuance then crediting insuance for payout', async function () {
-    let airline = accounts[0];
-    let consumer = accounts[5];
-    let flightNumber = "HJD157";
-    let insured = true;
-    let price = web3.utils.toWei("4");
-    let timestamp = Math.floor(Date.now() / 1000) + 5;
-    await config.flightSuretyApp.registerFlight(flightNumber,timestamp,price,{from:airline});
-    let result = await config.flightSuretyApp.getTicketPrice.call(flightNumber,timestamp,airline);
-    await config.flightSuretyApp.buyTicket(flightNumber,timestamp,airline,insured, {from:consumer,value:result[1]});
-    await sleep(6000);
-    result = await config.flightSuretyApp.creditAmount.call({from:consumer});
-    await config.flightSuretyApp.getCredit(flightNumber,timestamp,airline,{from:consumer});
-    result = await config.flightSuretyApp.creditAmount.call({from:consumer});
-    price = web3.utils.fromWei(result)
-    assert.equal(price,6,"credit about is not correct");
-    await config.flightSuretyApp.receiveCredit({from:consumer});
-    result = await config.flightSuretyApp.creditAmount.call({from:consumer});
-    assert.equal(result,0,"Funds where unsuccessfully");
-  });
 
 });
